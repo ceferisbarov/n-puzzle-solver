24e6191ec63ccb849ea15c3a6c5676a3a6155df2
Jafar Isbarov <cefer.isbarov@gmail.com>
2024-02-01 21:35:10 +0400

    refactoring

--- src/astart_agent.py
+++ src/astart_agent.py
@@ -1,18 +1,43 @@
-from base_agent import BaseNPuzzleSolver
 from copy import deepcopy
-from utils import pprint
+from utils import pprint, MIN_N, MAX_N
+from state import State
 
-class AStartAgent(BaseNPuzzleSolver):
+class AStartAgent(object):
 	"""
 	This is an implementation of A* based N-puzzle agent.
 	"""
 	def __init__(self, filepath) -> None:
-		super().__init__(filepath)
+		self.state = self.read_input(filepath)
+		assert self.state.is_solvable(), "This matrix has no solution!"
+
 		self.hashtable = {self.state.id: self.state}
 		self.open = [self.state.id]
 		self.closed = []
 		self.final_state_id = self.get_final_state_id()
 	
+	def read_input(self, filepath):
+		"""
+		Given a filepath:
+		(1) parses contens of this file into a 2D list,
+		(2) validates the content
+		and returns it.
+		"""
+		with open(filepath, "r") as f:
+			input_lines = f.readlines()
+
+		n = len(input_lines)
+
+		if n < MIN_N or n > MAX_N:
+			raise ValueError("Invalid matrix size!")
+		
+		input_matrix = []
+
+		for line in input_lines:
+			values = list(map(int, line.split("\t")))
+			input_matrix.append(values)
+
+		return State(input_matrix)
+
 	def get_final_state_id(self):
 		"""
 		Create the desired final state id.
